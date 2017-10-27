a31812593da210b411d12bdf2af67fb72b5adca8
Shlomi Fish <shlomif@shlomifish.org>
2017-10-28 01:08:57 +0300

    Render.

---
+++ bin/render-markdown.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Text::Markdown qw/ markdown /;
+use Path::Tiny qw/ path /;
+use Text::WrapAsUtf8 qw/ print_utf8 /;
+
+print_utf8(
+    <<"EOF",
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE
+    html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US">
+<head>
+<title>Static Site Gens</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+EOF
+    markdown( path( shift @ARGV )->slurp_utf8 ),
+    <<"EOF",
+</body>
+</html>
+EOF
+);
